1a61025e9d27c0b059d7b602b12a46a56ed009e4
Marco Veeneman <marco-veeneman@hotmail.com>
2022-08-26 19:34:50 +0200

    Change platform name to am43-ble

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const AM43Platform = require("./lib/AM43Platform.js")
 
 module.exports = function (api) {
-    api.registerPlatform('homebridge-am43-blinds', 'am43-blinds', AM43Platform)
+    api.registerPlatform('homebridge-am43-ble', 'am43-ble', AM43Platform)
 }
